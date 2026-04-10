7e8ef6ab34d73e049077349620eb128bbef758bb
Ubuntu <ubuntu@ip-172-31-23-84.ec2.internal>
2026-04-10 15:20:20 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+## providers block
+
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_security_group" "mysecuritygroup0410" {
+ingress {
+from_port = "80"
+to_port = "80"
+cidr_blocks = ["0.0.0.0/0"]
+}
+}
+
+resource "aws_instance" "myinstance" {
+ami                    = "ami-04680790a315cd58d"
+instance_type          = "t2.micro"
+vpc_security_group_ids = [aws_security_group.mysecuritygroup0410.id]
+tags = {
+name = "forghamachine0410"
+
+}
+}
+
+
+
